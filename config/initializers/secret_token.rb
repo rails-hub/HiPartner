27f33ba1dd1081234e6e3653c7f6b4954a941c15
saqib <saqib@saqib.(none)>
2014-02-28 23:04:27 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HiPartner::Application.config.secret_key_base = '61796e43ac9c449d43dd4a11477f8ab114697644f479de6e2359dc7a45fcc5a6115a5cd5198125d8ab0ebc2e7bb68b7985fc5cef14980398134a28061cfb296e'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+HiPartner::Application.routes.draw do
+  root :to => "home#index"
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :users
+end
